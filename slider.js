951aa1f832bc0fbc219b6dd099b7706439257986
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-01-15 22:46:03 +0200

    Added CSS and updated HTML structure

---
+++ slider.js
@@ -0,0 +1,31 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.image');
+
+let slideNumber = 1;
+
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;
+}
+const getFirstSlide = () => {
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+}
+
+
+right.addEventListener("click", () => {
+    if (slideNumber < length) {
+        nextSlide();
+    } else {
+        getFirstSlide();
+    }
+
+});
+
+left.addEventListener("click", () => {
+
+});
