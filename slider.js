6d5ec9ea3003c1522277dc2645034337dc2a9186
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-01-15 23:24:41 +0200

    added event Listeners for slider and buttons

--- slider.js
+++ slider.js
@@ -2,18 +2,59 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.image');
+const bottom = document.querySelector('.bottom');
+
 
 let slideNumber = 1;
 
 const length = images.length;
 
+
+for (let i = 0; i < length; i++) {
+    const div = document.createElement("div");
+    div.className = "button";
+    bottom.appendChild(div)
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "green";
+
+
+const resetBackGround = () => {
+    buttons.forEach(button => {
+        button.style.backgroundColor = "transparent";
+    });
+};
+
+buttons.forEach((button, i) => {
+    button.addEventListener('click', () => {
+        resetBackGround();
+        slider.style.transform = `translateX(-${i * 800}px)`
+        slideNumber = i + 1;
+        button.style.backgroundColor = "green";
+    })
+})
+
 const nextSlide = () => {
     slider.style.transform = `translateX(-${slideNumber * 800}px)`;
     slideNumber++;
 }
+const prevSlide = () => {
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+    slideNumber--;
+}
 const getFirstSlide = () => {
     slider.style.transform = `translateX(0px)`;
     slideNumber = 1;
+}
+const getLastSlide = () => {
+    slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+    slideNumber = length;
+}
+
+const changeColor = () => {
+    resetBackGround();
+    buttons[slideNumber - 1].style.backgroundColor = "green";
 }
 
 
@@ -23,9 +64,14 @@ right.addEventListener("click", () => {
     } else {
         getFirstSlide();
     }
-
+    changeColor();
 });
 
 left.addEventListener("click", () => {
-
+    if (slideNumber > 1) {
+        prevSlide();
+    } else {
+        getLastSlide();
+    }
+    changeColor();
 });
